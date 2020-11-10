e20337b0e0ea71192a31f4fb6102668f96c0d4a0
fsc5642 <74257372+fsc5642@users.noreply.github.com>
2020-11-11 01:01:31 +0900

    first version

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Web</title>
+  </head>
+  <body>
+    <h1><a href="index.php">WEB</a><h1>
+    <ol>
+      <li><a href="1.php">HTML</a></li>
+      <li><a href="2.php">CSS</a></li>
+      <li><a href="3.php">JavaScript</a></li>
+    </ol>
+    <h2>Web 이란 무엇인가?</h2>
+    <p>dfjkafjknnnfdfj</p>
+
+  </body>
+</html>
